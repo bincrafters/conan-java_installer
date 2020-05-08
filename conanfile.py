aae08845d1dfed8efdca48d2c968aee4608ce282
Rob Boehne <robb@datalogics.com>
2020-05-08 18:12:55 -0500

    Create a fork of the java_installer that packagtes the JDK from Azul.com Correct the recipe to set os and arch instead of os_build, arch_build, and update to the latest version of the JDK."

--- conanfile.py
+++ conanfile.py
@@ -1,71 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from conans import ConanFile, tools
 import os
 import platform
+from conans import ConanFile, tools
 
 
 class JavaInstallerConan(ConanFile):
-    name = "java_installer"
-    # x86 is 8.0.153, while x86_64 is 8.0.152
-    version = "8.0.153"
-    url = "https://github.com/bincrafters/conan-java_installer"
-    description = "Java installer distributed via Conan"
-    license = "https://www.azul.com/products/zulu-and-zulu-enterprise/zulu-terms-of-use/"
-    settings = "os_build", "arch_build"
+    name = 'java_installer'
+    version = '8.0.252'
+    url = 'https://github.com/bincrafters/conan-java_installer'
+    description = 'Java installer distributed via Conan'
+    license = 'https://www.azul.com/products/zulu-and-zulu-enterprise/zulu-terms-of-use/'
+    settings = 'os', 'arch'
 
     @property
     def jni_folder(self):
-        folder = {"Linux": "linux", "Darwin": "darwin", "Windows": "win32"}.get(platform.system())
-        return os.path.join("include", folder)
+        folder = {'Linux': 'linux', 'Darwin': 'darwin',
+                  'Windows': 'win32'}.get(platform.system())
+        return os.path.join('include', folder)
 
     def config_options(self):
         # Checking against self.settings.* would prevent cross-building profiles from working
-        if self.settings.arch_build not in ["x86_64", "x86"]:
-            raise Exception("Unsupported Architecture.  This package currently only supports x86_64 and x86.")
-        if self.settings.os_build not in ["Windows", "Macos", "Linux"]:
-            raise Exception("Unsupported System. This package currently only support Linux/Darwin/Windows")
-        if self.settings.os_build == "Macos" and self.settings.arch_build == "x86":
-            raise Exception("Unsupported System (32-bit Mac OS X)")
+        if self.settings.arch not in ['x86_64', 'x86']:
+            raise Exception(
+                'Unsupported Architecture.  This package currently only supports x86_64 and x86.')
+        if self.settings.os not in ['Windows', 'Macos', 'Linux']:
+            raise Exception(
+                'Unsupported System. This package currently only support Linux/Darwin/Windows')
+        if self.settings.os == 'Macos' and self.settings.arch == 'x86':
+            raise Exception('Unsupported System (32-bit Mac OS X)')
 
     def build(self):
-        x64 = self.settings.arch_build == 'x86_64'
-        source_file = "zulu8.{0}-jdk{1}-{2}_{3}"
-        zulu_version = '25.0.1' if x64 else '25.0.3'
-        version = '8.0.152' if x64 else '8.0.153'
+        x64 = self.settings.arch == 'x86_64'
+        source_file = 'zulu8.{0}-ca-jdk{1}-{2}_{3}'
+        zulu_version = '46.0.19'
+        ext = 'tar.gz'
         arch = 'x64' if x64 else 'i686'
+        if self.settings.os == 'Windows':
+            source_file = source_file.format(
+                zulu_version, self.version, 'win', arch)
+            ext = 'zip'
+            if x64:
+                checksum = '993ef31276d18446ef8b0c249b40aa2dfcea221a5725d9466cbea1ba22686f6b'
+            else:
+                checksum = '44fa7abc0f647a014b2c6a6cf78000cc2a554b15132ea83e60229ea58a77c551'
+        elif self.settings.os == 'Linux':
+            source_file = source_file.format(
+                zulu_version, self.version, 'linux', arch)
+            if x64:
+                checksum = 'ab8a4194006f12dd48bf7f176ca7879706d3f8fc7d3208313a46cc9ee2270716'
+            else:
+                checksum = 'bba0ec1606823515172e3eee9fcaa9ea29d51be49ee903c4b1e708af3e60a29f'
+        elif self.settings.os == 'Macos':
+            source_file = source_file.format(
+                zulu_version, self.version, 'macosx', arch)
+            checksum = '43570b0a6455a02d25b0c4937164560fdb0a9478f9010c583f510fa80881ce0b'
 
-        if self.settings.os_build == 'Windows':
-            source_file = source_file.format(zulu_version, version, "win", arch)
-            ext = "zip"
-            checksum = "754bd10d29212c817dfad8758a8df9bc" if x64 else "67752bd3cb8356215900883b55a8c26c"
-        elif self.settings.os_build == 'Linux':
-            source_file = source_file.format(zulu_version, version, "linux", arch)
-            ext = "tar.gz"
-            checksum = "cc6e9ff13c27d27033220208d5450f2d" if x64 else "99da96ba61ccb53aab85261d4746b51c"
-        elif self.settings.os_build == 'Macos':
-            source_file = source_file.format(zulu_version, version, "macosx", arch)
-            ext = "tar.gz"
-            checksum = "0348962b47bf5197e7b5e78cfd073d84"
-
-        bin_filename = "{0}.{1}".format(source_file, ext)
-        download_url = "http://cdn.azul.com/zulu/bin/{0}".format(bin_filename)
-        self.output.info("Downloading : {0}".format(download_url))
-        tools.get(download_url, md5=checksum)
-        os.rename(source_file, "sources")
+        bin_filename = '{0}.{1}'.format(source_file, ext)
+        download_url = 'http://cdn.azul.com/zulu/bin/{0}'.format(bin_filename)
+        self.output.info('Downloading : {0}'.format(download_url))
+        tools.get(download_url, sha256=checksum)
+        os.rename(source_file, 'sources')
 
     def package(self):
-        self.copy(pattern="*", dst=".", src="sources")
+        self.copy(pattern='*', dst='.', src='sources')
 
     def package_info(self):
         self.cpp_info.includedirs.append(self.jni_folder)
 
         java_home = os.path.join(self.package_folder)
-        bin_path = os.path.join(java_home, "bin")
+        bin_path = os.path.join(java_home, 'bin')
 
-        self.output.info("Creating JAVA_HOME environment variable with : {0}".format(java_home))
+        self.output.info(
+            'Creating JAVA_HOME environment variable with : {0}'.format(java_home))
         self.env_info.JAVA_HOME = java_home
-        
-        self.output.info("Appending PATH environment variable with : {0}".format(bin_path))
-        self.env_info.path.append(bin_path)
+
+        self.output.info(
+            'Appending PATH environment variable with : {0}'.format(bin_path))
+        self.env_info.PATH.append(bin_path)
